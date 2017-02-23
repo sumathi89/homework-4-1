dca4d02c221fc656d1199d013effa9613d20601e
Sumathi Selvaraj <ss3467@afsconnect1.njit.edu>
2017-02-23 01:07:32 -0500

    Show Categories

--- category_list.php
+++ category_list.php
@@ -30,8 +30,16 @@ $statement->closeCursor();
         </tr>
         
         <!-- add code for the rest of the table here -->
-    
-    </table>
+  <?php foreach ($categories as $category) : ?>
+  <tr>
+     <td><?php echo $category['categoryName']; ?></td>
+     <td><form action="delete_category.php" method="post">
+     <input type="hidden" name="category_id"  value="<?php echo $category['categoryID']; ?>">
+     <input type="submit" value="Delete">
+     </form></td>
+  </tr>
+  <?php endforeach; ?>
+  </table>
 
     <h2>Add Category</h2>
     
